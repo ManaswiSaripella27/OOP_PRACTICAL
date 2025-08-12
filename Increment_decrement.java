6131cc3b816d7fad274072f92affd47ea7e5ff9f
ManaswiSaripella27 <manaswi.saripella18@gmail.com>
2025-08-12 12:36:01 +0530

    Add files via upload

---
+++ Increment_decrement.java
@@ -0,0 +1,28 @@
+package CSE;
+import java.util.Scanner;
+public class Increment_decrement {
+	public static void main(String[] args) {
+
+	      Scanner input = new Scanner(System.in);
+
+	      System.out.print("Enter value for a: ");
+	      int a = input.nextInt();
+
+	      System.out.print("Enter value for b: ");
+	      int b = input.nextInt();
+
+	      int result1, result2;
+
+	      System.out.println("Original value of a: " + a);
+
+	      result1 = ++a;
+	      System.out.println("After incrementing a: " + result1);
+
+	      System.out.println("Original value of b: " + b);
+
+	      result2 = --b;
+	      System.out.println("After decrementing b: " + result2);
+
+	      input.close();
+	    }
+}
